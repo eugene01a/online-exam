b71557a2f4501bb6631c83ab590a486d5128de9e
Eugene Ichinose <eichinose@telesign.com>
2019-07-18 14:05:00 -0700

    integrating Flask and Angular

---
+++ frontend/src/app/exams/exams-api.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import {API_URL} from '../env';
+import {Exam} from './exam.model';
+import { throwError } from 'rxjs';
+
+@Injectable()
+export class ExamsApiService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  private static _handleError(err: HttpErrorResponse | any) {
+    return throwError(err.message || 'Error: Unable to complete request.');
+  }
+
+  // GET list of public, future events
+  getExams(): Observable<Exam[]> {
+    return this.http
+      .get < Exam[] > (`${API_URL}/exams`)
+      .catch(ExamsApiService._handleError);
+  }
+}
