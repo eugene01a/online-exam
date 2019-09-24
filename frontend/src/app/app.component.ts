2dcc844acdada42fa94e768be9e59c2647fffa96
Eugene Ichinose <eichinose@telesign.com>
2019-09-24 14:59:15 -0700

    adding modal integration

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,34 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-import * as Auth0 from 'auth0-web';
+import {ModalService} from "./_services";
+
+import './_content/app.less';
+import './_content/modal.less';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <mat-toolbar color="primary" class="mat-elevation-z10">
-      <button mat-button>Online Exams</button>
-      <button mat-button>About</button>
+  templateUrl: 'app.component.html'
 
-      <!-- This fills the remaining space of the current row -->
-      <span class="fill-remaining-space"></span>
-
-      <button mat-button (click)="signIn()" *ngIf="!authenticated">Sign In</button>
-      <button mat-button (click)="signOut()" *ngIf="authenticated">Sign Out</button>
-    </mat-toolbar>
-
-    <div class="view-container">
-  <router-outlet></router-outlet>
-</div>
-  `,
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   authenticated = false;
 
-  signIn = Auth0.signIn;
-  signOut = Auth0.signOut;
+  constructor(private modalService: ModalService) {
+  }
 
   ngOnInit() {
-    const self = this;
-    Auth0.subscribe((authenticated) => (self.authenticated = authenticated));
   }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule, MatButtonModule, MatCardModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
 import * as Auth0 from 'auth0-web';
 import {CallbackComponent} from './callback.component';
 import {BrowserModule} from '@angular/platform-browser';
@@ -10,11 +10,15 @@ import {ExamsApiService} from './exams/exams-api.service';
 import {ExamFormComponent} from './exams/exam-form.component';
 import {RouterModule, Routes} from '@angular/router';
 import {ExamsComponent} from './exams/exams.component';
+import {FormsModule} from '@angular/forms';
+import {ModalComponent} from './_directives';
+import {ModalService} from './_services';
+
 
 const appRoutes: Routes = [
-  { path: 'callback', component: CallbackComponent },
-  { path: 'new-exam', component: ExamFormComponent },
-  { path: '', component: ExamsComponent },
+  {path: 'callback', component: CallbackComponent},
+  {path: 'new-exam', component: ExamFormComponent},
+  {path: '', component: ExamsComponent},
 ];
 
 @NgModule({
@@ -22,7 +26,8 @@ const appRoutes: Routes = [
     AppComponent,
     ExamFormComponent,
     ExamsComponent,
-    CallbackComponent
+    CallbackComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,18 +38,52 @@ const appRoutes: Routes = [
     NoopAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    FormsModule,
   ],
-  providers: [ExamsApiService],
+  providers: [ExamsApiService, ModalService],
   bootstrap: [AppComponent]
 })
-export class AppModule {constructor() {
-    Auth0.configure({
-      domain: 'dev-s5ay29rq.auth0.com',
-      audience: 'https://dev-s5ay29rq.auth0.com/api/v2/',
-      clientID: 'T0fB92fSn7yTXou5c94w8hBY0R5nqBHG',
-      redirectUri: 'http://localhost:4200/callback',
-      scope: 'openid profile manage:exams'
-    });
-  }
+export class AppModule {
+  // constructor() {
+  //   Auth0.configure({
+  //     domain: 'dev-s5ay29rq.auth0.com',
+  //     audience: 'https://dev-s5ay29rq.auth0.com/api/v2/',
+  //     clientID: 'T0fB92fSn7yTXou5c94w8hBY0R5nqBHG',
+  //     redirectUri: 'http://localhost:4200/callback',
+  //     scope: 'openid profile manage:exams'
+  //   });
+  // }
 }
+
+// import { NgModule }      from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule }    from '@angular/forms';
+//
+// import { AppComponent }  from './app.component';
+// import { routing }        from './app.routing';
+//
+// import { ModalComponent } from './_directives';
+// import { ModalService } from './_services';
+// import { HomeComponent } from './home';
+// import { TestPageComponent } from './test-page';
+//
+// @NgModule({
+//     imports: [
+//         BrowserModule,
+//         FormsModule,
+//         routing
+//     ],
+//     declarations: [
+//         AppComponent,
+//         ModalComponent,
+//         HomeComponent,
+//         TestPageComponent
+//     ],
+//     providers: [
+//         ModalService
+//     ],
+//     bootstrap: [AppComponent]
+// })
+//
+// export class AppModule { }
